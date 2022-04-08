cf80147217c478f32ffc41a83ae330ea29a4e8bd
BENEDICTA55 <okerekebenedicta55@gmail.com>
2022-04-08 15:49:22 -0700

    My print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,19 +2,19 @@
 /**
  *main - main block
  *Description: prints the alphabet in lowercase,
- *and then in uppercase, followed by a new line.
+ *and then uppercase, followed by a new line.
  *Return: 0
  */
 int main(void)
 {
-char c = 'a';
-while (c <= 'Z')
+char c ='a';
+while (c <= 'z')
 {
 putchar(c);
 c++;
 }
-c = 'A';
-while (c <= 'Z')
+c = 'a';
+while (c <= 'z')
 {
 putchar(c);
 c++;
