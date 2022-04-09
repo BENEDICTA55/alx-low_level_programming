0f5bcd4066bbccb6f6474da8d2993a6f2ff50228
BENEDICTA55 <okerekebenedicta55@gmail.com>
2022-04-08 21:10:17 -0700

    My 8 print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+char digit;
+for (digit = '0'; digit <= '9'; digit++)
+putchar(digit);
+for (digit = 'a'; digit <== 'f'; digit++)
+putchar('\n');
+return (0);
+}
