d62bab3662349cae9bf8b2d308e9ba79a50db494
BENEDICTA55 <okerekebenedicta55@gmail.com>
2022-04-15 00:07:52 -0700

    My commit message

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,17 @@
 #include "main.h"
+void positive_or_negative(int i)
+
 /**
  *main - main
  *Return: 0
  */
+
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
 }
